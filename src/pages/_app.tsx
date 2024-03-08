2f110f162ace485fab0bbce1abdc770157df638b
Inter <me@inttter.com>
2024-03-08 22:10:27 +0000

    🔍 chore(seo): use vercel analytics & speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
 import Head from '../components/Layout';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
       <Component {...pageProps} />
+      <SpeedInsights />
+      <Analytics />
       </Head>
     </>
   );
