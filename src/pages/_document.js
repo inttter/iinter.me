beb41635824cde6d6ef78c90f66b97339ac58ac7
Inter <hi@iinter.me>
2024-03-23 16:48:46 +0000

    ✨ feat: show blog post info in ograph
    * Also fixed the normal ograph things 🐛

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,37 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
+    const { post } = this.props.__NEXT_DATA__.props.pageProps;
+    const isBlogPostPage = !!post;
+
     return (
       <Html>
         <Head>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+          {isBlogPostPage && (
+            <>
+              <meta property="og:image" content={post.frontmatter.image} />
+              <meta name="twitter:image" content={post.frontmatter.image} />
+              <meta property="og:description" content={post.frontmatter.description} />
+            </>
+          )}
+          {!isBlogPostPage && (
+            <>
+              <meta property="og:image" content="https://iinter.me/external-assets/og.png" />
+              <meta name="twitter:image" content="https://iinter.me/external-assets/og.png" />
+              <meta property="og:description" content="games + cats + computers." />
+            </>
+          )}
           <meta property="og:title" content="inter" />
-          <meta property="og:image" content="https://inttter.com/assets/external-assets/og.png" />
           <meta property="og:site_name" content="iinter.me" />
           <meta property="og:url" content="iinter.me" />
           <meta property="og:type" content="website" />
-          <meta property="og:description" content="games + cats + computers." />
           <meta name="theme-color" content="#0A0A0A" />
         </Head>
         <body>
@@ -23,4 +43,4 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument
+export default MyDocument;
