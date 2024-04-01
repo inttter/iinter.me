273ce5852b22641726ad429da8fb577e2e735948
Inter <hi@iinter.me>
2024-04-02 00:44:17 +0100

    ✨ feat: add head title to projects page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Projects from '../../components/Projects';
 import Link from 'next/link';
+import Head from 'next/head';
 import '@fontsource/geist-sans';
 import '@fontsource/geist-sans/700.css';
 import '@fontsource/geist-sans/600.css';
@@ -10,6 +11,9 @@ export default function ProjectsPage() {
     <div className="bg-neutral-900 min-h-screen flex flex-col justify-center items-center antialiased scroll-smooth p-4 md:p-8">
       <div className="max-w-2xl w-full px-4 py-8 space-y-6 flex-col">
         <div className="flex items-center justify-start">
+        <Head>
+          <title>inter | projects</title>
+        </Head>
           <Projects />
         </div>
         <div className="absolute top-2 space-x-4">
