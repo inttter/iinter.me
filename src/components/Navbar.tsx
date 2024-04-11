bca1d3af89da0c53b3606fdec64908356239f238
Inter <hi@iinter.me>
2024-04-11 15:46:12 +0100

    💄 style: add close icon to mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
+import { MdOutlineClose } from "react-icons/md";
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -24,24 +25,24 @@ function Navbar() {
                 transition={{ duration: 0.3 }}
                 className={`${isOpen ? 'block' : 'hidden'} bg-neutral-800 p-4 my-4 rounded-lg shadow-lg z-10`}
             >
-                <div className="flex flex-col space-y-2 hover:text-zinc-100">
-                    <Link href="/" className="text-neutral-300 hover:text-neutral-100">
+                <div className="flex flex-col space-y-2 text-neutral-300 hover:text-neutral-100 duration-300">
+                    <Link href="/" className="text-neutral-300 hover:text-neutral-100 duration-300">
                         Home
                     </Link>
-                    <Link href="/about" className="text-neutral-300 hover:text-neutral-100">
+                    <Link href="/about" className="text-neutral-300 hover:text-neutral-100 duration-300">
                         About
                     </Link>
-                    <Link href="/projects" className="text-neutral-300 hover:text-neutral-100">
+                    <Link href="/projects" className="text-neutral-300 hover:text-neutral-100 duration-300">
                         Projects
                     </Link>
-                    <Link href="/blog" className="text-neutral-300 hover:text-neutral-100">
+                    <Link href="/blog" className="text-neutral-300 hover:text-neutral-100 duration-300">
                         Blog
                     </Link>
                     <button
-                        className="text-neutral-300 hover:text-neutral-100 -ml-4"
+                        className="flex items-center text-neutral-300 hover:text-neutral-100 duration-300 -ml-1"
                         onClick={toggleMenu}
                     >
-                        Close
+                        <MdOutlineClose size={20} className="mr-1" /> Close
                     </button>
                 </div>
             </motion.div>
