819162e33b6bebd759662ceb916d99432111741d
Inter <hi@iinter.me>
2024-04-13 17:46:35 +0100

    💄 style: add hover animation to projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,7 +21,7 @@ function Projects() {
       >
         Projects
       </motion.div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-x-3 gap-y-1">
         {projectsData.map((project, index) => (
           <motion.div
             key={index}
@@ -30,51 +30,53 @@ function Projects() {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5, delay: index * 0.1 }}
           >
-            <div className="flex items-center">
-              <Link
-                href={project.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-zinc-300 border-b-2 border-gray-500 hover:border-gray-400 hover:text-zinc-100 duration-300 animate-in fade-in"
-              >
-                {project.name}
-              </Link>
-              {project.github && (
+            <div className="hover:bg-neutral-800 hover:bg-opacity-70 hover:shadow-2xl hover:shadow-neutral-700 duration-300 p-3 rounded-lg">
+              <div className="flex items-center">
                 <Link
-                  href={project.github}
+                  href={project.link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-neutral-600 hover:text-neutral-400 duration-300 ml-2 tooltip tooltip-bottom bg-transparent"
-                  data-tip="View on GitHub"
-                  data-theme="lofi"
+                  className="text-zinc-300 border-b-2 border-gray-500 hover:border-gray-400 hover:text-zinc-100 duration-300 animate-in fade-in"
                 >
-                  <FaGithub />
+                  {project.name}
                 </Link>
-              )}
-              {!project.maintained && (
-                <motion.div
-                  className="bg-[#383131] text-red-200 p-1 px-2 ml-2 rounded-md text-xs flex items-center tooltip tooltip-top cursor-help"
-                  data-tip="This project has been deprecated. While it may still work, updates will no longer be provided to it. For more info, visit the repository."
-                  data-theme="lofi"
-                  initial={{ scale: 0 }}
-                  animate={{ scale: 1 }}
-                  transition={{ duration: 0.5, delay: index * 0.1 }}
-                >
-                  <div className="hover:blur-[1px] duration-300 flex items-center">
-                    <CgDanger className="mr-1" />
-                    deprecated
-                  </div>
-                </motion.div>
-              )}
+                {project.github && (
+                  <Link
+                    href={project.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-neutral-600 hover:text-neutral-400 duration-300 ml-2 tooltip tooltip-bottom bg-transparent"
+                    data-tip="View on GitHub"
+                    data-theme="lofi"
+                  >
+                    <FaGithub />
+                  </Link>
+                )}
+                {!project.maintained && (
+                  <motion.div
+                    className="bg-[#383131] text-red-200 p-1 px-2 ml-2 rounded-md text-xs flex items-center tooltip tooltip-top cursor-help"
+                    data-tip="This project has been deprecated. While it may still work, updates will no longer be provided to it. For more info, visit the repository."
+                    data-theme="lofi"
+                    initial={{ scale: 0 }}
+                    animate={{ scale: 1 }}
+                    transition={{ duration: 0.5, delay: index * 0.1 }}
+                  >
+                    <div className="hover:blur-[1px] duration-300 flex items-center">
+                      <CgDanger className="mr-1" />
+                      deprecated
+                    </div>
+                  </motion.div>
+                )}
+              </div>
+              <motion.p
+                className="text-zinc-300 text-sm py-2.5"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: index * 0.1 }}
+              >
+                {project.description}
+              </motion.p>
             </div>
-            <motion.p
-              className="text-zinc-300 text-sm py-2.5"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5, delay: index * 0.1 }}
-            >
-              {project.description}
-            </motion.p>
           </motion.div>
         ))}
       </div>
