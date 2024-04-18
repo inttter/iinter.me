d51b63551615fc90bfade5c394c2a953109c0004
Inter <hi@iinter.me>
2024-04-18 22:32:18 +0100

    💄 style: update styling of projects page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -30,13 +30,13 @@ function Projects() {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5, delay: index * 0.1 }}
           >
-            <div className="hover:bg-neutral-800 hover:bg-opacity-70 hover:shadow-2xl hover:shadow-neutral-700 hover:border-neutral-700 border-transparent border-2 duration-300 p-3 rounded-lg">
+            <div className="hover:bg-neutral-900 hover:bg-opacity-70 hover:shadow-2xl hover:shadow-neutral-800 hover:border-neutral-800 border-transparent border-2 duration-300 p-3 rounded-md">
               <div className="flex items-center">
                 <Link
                   href={project.link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-zinc-300 border-b-2 border-gray-500 hover:border-gray-400 hover:text-zinc-100 duration-300 animate-in fade-in"
+                  className="text-zinc-100 hover:text-zinc-300 duration-300 border-b-2 border-dotted hover:border-solid border-neutral-700 hover:border-neutral-600"
                 >
                   {project.name}
                 </Link>
@@ -52,22 +52,22 @@ function Projects() {
                     <FaGithub />
                   </Link>
                 )}
-                {!project.maintained && (
-                  <motion.div
-                    className="bg-[#383131] text-red-200 p-1 px-2 ml-2 rounded-md text-xs flex items-center tooltip tooltip-top cursor-help"
-                    data-tip="This project has been deprecated. While it may still work, updates will no longer be provided to it. For more info, visit the repository."
-                    data-theme="lofi"
-                    initial={{ scale: 0 }}
-                    animate={{ scale: 1 }}
-                    transition={{ duration: 0.5, delay: index * 0.1 }}
-                  >
-                    <div className="hover:blur-[1px] duration-300 flex items-center">
-                      <CgDanger className="mr-1" />
-                      deprecated
-                    </div>
-                  </motion.div>
-                )}
               </div>
+              {!project.maintained && (
+                <motion.div
+                  className="bg-[#383131] text-red-200 p-1 px-2 mt-2.5 rounded-md text-xs flex items-center tooltip tooltip-top cursor-help "
+                  data-tip="This project has been deprecated. While it may still work, updates will no longer be provided to it. For more info, visit the repository."
+                  data-theme="lofi"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: index * 0.1 }}
+                >
+                  <div className="hover:blur-[1px] duration-300 flex items-center">
+                    <CgDanger className="mr-1" />
+                    Deprecated
+                  </div>
+                </motion.div>
+              )}
               <motion.p
                 className="text-zinc-300 text-sm py-2.5"
                 initial={{ opacity: 0 }}
