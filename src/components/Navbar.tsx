791260498d2e6869f34d6811f96997cc981d4ca9
Inter <hi@iinter.me>
2024-04-18 21:48:56 +0100

    💄 style: redo navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,50 +13,44 @@ function Navbar() {
     return (
         <div className="absolute top-10">
             <button
-                className="md:hidden text-neutral-500 hover:border-b-2 hover:border-neutral-500 selection:bg-[#E8D4B6] selection:text-black"
+                className="md:hidden text-zinc-100 hover:text-zinc-300 duration-300 selection:bg-[#E8D4B6] selection:text-black"
                 onClick={toggleMenu}
             >
-                Menu ↗
+                menu ↗
             </button>
             <motion.div
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: isOpen ? 1 : 0, y: isOpen ? 0 : -20 }}
                 exit={{ opacity: 0, y: -20 }}
                 transition={{ duration: 0.3 }}
-                className={`${isOpen ? 'block' : 'hidden'} bg-neutral-800 p-4 my-4 rounded-lg shadow-lg z-10`}
+                className={`${isOpen ? 'block' : 'hidden'} bg-neutral-950 bg-opacity-60 backdrop-blur-sm p-4 my-1 rounded-md shadow-lg z-10 border border-neutral-800`}
             >
                 <div className="flex flex-col space-y-2 text-neutral-300 hover:text-neutral-100 duration-300">
-                    <Link href="/" className="text-neutral-300 hover:text-neutral-100 duration-300">
+                    <Link href="/" className="text-neutral-100 hover:text-neutral-300 duration-300">
                         Home
                     </Link>
-                    <Link href="/about" className="text-neutral-300 hover:text-neutral-100 duration-300">
+                    <Link href="/about" className="text-neutral-100 hover:text-neutral-300 duration-300">
                         About
                     </Link>
-                    <Link href="/projects" className="text-neutral-300 hover:text-neutral-100 duration-300">
+                    <Link href="/projects" className="text-neutral-100 hover:text-neutral-300 duration-300">
                         Projects
                     </Link>
-                    <Link href="/blog" className="text-neutral-300 hover:text-neutral-100 duration-300">
+                    <Link href="/blog" className="text-neutral-100 hover:text-neutral-300 duration-300">
                         Blog
                     </Link>
-                    <button
-                        className="flex items-center text-neutral-300 hover:text-neutral-100 duration-300 -ml-1"
-                        onClick={toggleMenu}
-                    >
-                        <MdOutlineClose size={20} className="mr-1" /> Close
-                    </button>
                 </div>
             </motion.div>
             <div className="hidden md:flex space-x-4">
-                <Link href="/" className="border-b-2 border-gray-500 hover:border-gray-400 hover:text-zinc-200 duration-300 animate-in fade-in mx-1 selection:bg-[#E8D4B6] selection:text-black">
+                <Link href="/" className="text-zinc-100 hover:text-zinc-300 duration-300 border-b-2 border-dotted hover:border-solid border-neutral-700 hover:border-neutral-600">
                     /
                 </Link>
-                <Link href="/about" className="border-b-2 border-gray-500 hover:border-gray-400 hover:text-zinc-200 duration-300 animate-in fade-in mx-1 selection:bg-[#E8D4B6] selection:text-black">
+                <Link href="/about" className="text-zinc-100 hover:text-zinc-300 duration-300 border-b-2 border-dotted hover:border-solid border-neutral-700 hover:border-neutral-600">
                     about
                 </Link>
-                <Link href="/projects" className="border-b-2 border-gray-500 hover:border-gray-400 hover:text-zinc-200 duration-300 animate-in fade-in mx-1 selection:bg-[#E8D4B6] selection:text-black">
+                <Link href="/projects" className="text-zinc-100 hover:text-zinc-300 duration-300 border-b-2 border-dotted hover:border-solid border-neutral-700 hover:border-neutral-600">
                     projects
                 </Link>
-                <Link href="/blog" className="border-b-2 border-gray-500 hover:border-gray-400 hover:text-zinc-200 duration-300 animate-in fade-in mx-1 selection:bg-[#E8D4B6] selection:text-black">
+                <Link href="/blog" className="text-zinc-100 hover:text-zinc-300 duration-300 border-b-2 border-dotted hover:border-solid border-neutral-700 hover:border-neutral-600">
                     blog
                 </Link>
             </div>
