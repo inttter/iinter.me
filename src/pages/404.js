63c04f2d0bc22e6bce935774ad227198a348324f
Inter <hi@iinter.me>
2024-04-21 22:49:43 +0100

    🧹 chore: add symbol to 404 message for better indication

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { ShieldAlert } from 'lucide-react';
 
 export default function Custom404() {
   return (
     <div className="bg-main min-h-screen flex flex-col justify-center items-center antialiased scroll-smooth p-4 md:p-8">
       <div className="max-w-2xl w-full px-4 py-8 space-y-4 flex-col">
         <div className="flex items-center justify-start">
-          <h1 className="text-4xl md:text-5xl text-zinc-100 opacity-90 md:mt-0 mt-10 max-w-md font-semibold tracking-tighter border-b-2 border-dotted border-neutral-700">
+        <ShieldAlert size={40} className="mr-1 text-neutral-600 md:mt-0 mt-10" />
+          <h1 className="text-4xl md:text-5xl text-zinc-100 opacity-90 md:mt-0 mt-10 max-w-md font-semibold tracking-tighter border-b-2 border-dotted border-neutral-700 flex items-center">
             Page Not Found
           </h1>
         </div>
