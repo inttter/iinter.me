c287a9423a0dcf17f936d476f6b8ff2cf29b862f
Inter <hi@iinter.me>
2024-04-29 17:45:51 +0100

    🧹 chore: use better icon on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import { ShieldAlert } from 'lucide-react';
+import { CircleX } from 'lucide-react';
 
 export default function Custom404() {
   return (
     <div className="bg-main min-h-screen flex flex-col justify-center items-center antialiased scroll-smooth p-4 md:p-8">
       <div className="max-w-2xl w-full px-4 py-8 space-y-4 flex-col">
         <div className="flex items-center justify-start">
-        <ShieldAlert size={40} className="mr-1 text-neutral-600 md:mt-0 mt-10" />
+        <CircleX size={40} className="mr-2 text-red-400 md:mt-0 mt-10" />
           <h1 className="text-4xl md:text-5xl text-zinc-100 opacity-90 md:mt-0 mt-10 max-w-md font-semibold tracking-tighter border-b-2 border-dotted border-neutral-700 flex items-center">
             Page Not Found
           </h1>
