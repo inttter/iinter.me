d5b5bba603e76ef6d2c5622da436c1a34ad04760
Inter <hi@iinter.me>
2024-05-02 18:31:03 +0100

    ♻️ refactor: overhaul/improve projects page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { CgDanger } from "react-icons/cg";
-import { Github } from "lucide-react";
+import { Github, TriangleAlert } from "lucide-react";
 import projectsData from '../data/projects.json';
 
 function Projects() {
@@ -13,14 +13,6 @@ function Projects() {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
     >
-      <motion.div
-        className="text-3xl md:text-2xl font-semibold leading-none tracking-tight text-zinc-100 mb-4 px-3"
-        initial={{ y: -20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        Projects
-      </motion.div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-x-3 gap-y-1">
         {projectsData.map((project, index) => (
           <motion.div
@@ -30,46 +22,31 @@ function Projects() {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5, delay: index * 0.1 }}
           >
-            <div className="hover:bg-neutral-900 hover:bg-opacity-70 hover:shadow-2xl hover:shadow-neutral-800 hover:border-neutral-800 border-transparent border-2 duration-300 p-3 rounded-md">
+            <div className="hover:bg-gray-200 hover:bg-opacity-5 duration-200 p-3 rounded-md block hover:shadow-2xl hover:shadow-neutral-800 antialiased">
               <div className="flex items-center">
-                <Link
-                  href={project.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-zinc-100 hover:text-zinc-300 duration-300 border-b-2 border-dotted border-neutral-700 hover:border-neutral-500"
-                >
-                  {project.name}
+                <Link href={project.link} target="_blank" rel="noopener noreferer" passHref>
+                  <span className="text-zinc-100 hover:text-zinc-300 duration-300 border-b-2 border-dotted border-neutral-600 hover:border-neutral-500">
+                    {project.name}
+                  </span>
                 </Link>
-                {project.github && (
-                  <Link
-                    href={project.github}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-neutral-600 hover:text-neutral-400 duration-300 ml-2 tooltip tooltip-bottom bg-transparent"
-                    data-tip="View on GitHub"
-                    data-theme="lofi"
-                  >
-                    <Github size={20} />
-                  </Link>
-                )}
               </div>
               {!project.maintained && (
                 <motion.div
-                  className="bg-[#383131] text-red-200 p-1 px-2 mt-2.5 rounded-md text-xs flex items-center tooltip tooltip-top cursor-help "
-                  data-tip="This project has been deprecated. While it may still work, updates will no longer be provided to it. For more info, visit the repository."
+                  className="bg-orange-500 bg-opacity-50 text-zinc-300 p-1 px-2 mt-2.5 rounded-md text-xs flex items-center tooltip tooltip-top cursor-help"
+                  data-tip="While this project may still work, updates will no longer be provided to it, and any issues which arise will not be fixed."
                   data-theme="lofi"
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   transition={{ duration: 0.5, delay: index * 0.1 }}
                 >
                   <div className="hover:blur-[1px] duration-300 flex items-center">
-                    <CgDanger className="mr-1" />
-                    Deprecated
+                    <TriangleAlert size={15} strokeWidth="3" className="mr-1" />
+                    Deprecated Project
                   </div>
                 </motion.div>
               )}
               <motion.p
-                className="text-neutral-500 text-sm py-2.5"
+                className="text-stone-400 text-sm py-2.5"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 0.5, delay: index * 0.1 }}
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import Projects from '../../components/Projects';
 import Navbar from '../../components/Navbar';
-import Link from 'next/link';
 import Head from 'next/head';
 
 export default function ProjectsPage() {
   return (
-    <div className="bg-main min-h-screen flex flex-col justify-center items-center antialiased scroll-smooth p-4 md:p-8">
+    <div className="bg-main min-h-screen flex flex-col justify-center items-center antialiased p-4 md:p-8">
       <div className="max-w-2xl w-full px-4 py-8 space-y-6 flex-col">
         <div className="flex items-center justify-start">
         <Head>
           <title>projects | iinter.me</title>
         </Head>
         <div className="md:-py-0 py-20">
-        <Projects />
+          <Projects />
         </div>
-          <div className="fixed hidden md:block bottom-4 right-4 text-gray-500 text-xs">All projects are licensed under the 
-          <Link href="https://inter.mit-license.org/" className="ml-1 border-b-2 border-neutral-700 hover:border-neutral-600 duration-300 animate-in fade-in">
-            MIT License
-          </Link>
-          </div>
         </div>
         <Navbar />
       </div>
