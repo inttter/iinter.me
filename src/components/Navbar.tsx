2c973a4b38352db8495db51ae3cb7a721124ef70
Inter <hi@iinter.me>
2024-05-04 18:49:52 +0100

    💄 style: add icons to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
-import { motion } from 'framer-motion';
+import { ChevronRight, ChevronDown, NotebookPen, Code, Cat, Home } from 'lucide-react';
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,45 +12,41 @@ function Navbar() {
     return (
         <div className="absolute top-10">
             <button
-                className="md:hidden text-zinc-100 hover:text-zinc-300 duration-300"
+                className="md:hidden text-zinc-100 duration-300 flex items-center"
                 onClick={toggleMenu}
             >
-                menu ↗
+                menu {isOpen ? <ChevronDown className="text-emerald-300" /> : <ChevronRight className="text-neutral-600" />}
             </button>
-            <motion.div
-                initial={{ opacity: 0, y: -20 }}
-                animate={{ opacity: isOpen ? 1 : 0, y: isOpen ? 0 : -20 }}
-                exit={{ opacity: 0, y: -20 }}
-                transition={{ duration: 0.3 }}
-                className={`${isOpen ? 'block' : 'hidden'} bg-neutral-950 bg-opacity-60 backdrop-blur-sm p-4 my-1 rounded-md shadow-lg z-10 border border-neutral-800`}
+            <div
+                className={`${isOpen ? 'block' : 'hidden'} bg-neutral-900 bg-opacity-60 backdrop-blur-sm p-4 my-2 rounded-md shadow-2xl shadow-neutral-900 z-10 border border-neutral-800 animate-blurred-fade-in duration-300`}
             >
                 <div className="flex flex-col space-y-2 text-neutral-300 hover:text-neutral-100 duration-300">
                     <Link href="/" className="text-neutral-100 hover:text-neutral-300 duration-300 flex items-center">
-                        <Image src="https://us-east-1.tixte.net/uploads/inter.tixte.co/blossom_v2.webp" width={15} height={15} alt="Tree" className="mr-2" /> Home
+                        <Home size={15} className="mr-1.5 text-neutral-600" /> Home
                     </Link>
-                    <Link href="/anime" className="text-neutral-100 hover:text-neutral-300 duration-300">
-                        Anime List
+                    <Link href="/anime" className="text-neutral-100 hover:text-neutral-300 duration-300 flex items-center">
+                        <Cat size={15} className="mr-1.5 text-neutral-600" /> Anime List
                     </Link>
-                    <Link href="/projects" className="text-neutral-100 hover:text-neutral-300 duration-300">
-                        Projects
+                    <Link href="/projects" className="text-neutral-100 hover:text-neutral-300 duration-300 flex items-center">
+                       <Code size={15} className="mr-1.5 text-neutral-600" /> Projects
                     </Link>
-                    <Link href="/writing" className="text-neutral-100 hover:text-neutral-300 duration-300">
-                        Writing
+                    <Link href="/writing" className="text-neutral-100 hover:text-neutral-300 duration-300 flex items-center">
+                       <NotebookPen size={15} className="mr-1.5 text-neutral-600" /> Writing
                     </Link>
                 </div>
-            </motion.div>
+            </div>
             <div className="hidden md:flex space-x-1 items-center">
-                <Link href="/" className="text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1">
-                    /
+                <Link href="/" className="text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1 flex items-center">
+                    <Home size={15} className="mr-1.5 text-neutral-600" /> home
                 </Link>
-                <Link href="/anime" className="text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1">
-                    anime
+                <Link href="/anime" className="text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1 flex items-center">
+                   <Cat size={15} className="mr-1.5 text-neutral-600" /> anime
                 </Link>
-                <Link href="/projects" className="text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1">
-                    projects
+                <Link href="/projects" className="text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1 flex items-center">
+                   <Code size={15} className="mr-1.5 text-neutral-600" /> projects
                 </Link>
-                <Link href="/writing" className="text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1">
-                    writing
+                <Link href="/writing" className="text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1 flex items-center">
+                   <NotebookPen size={15} className="mr-1.5 text-neutral-600" /> writing
                 </Link>
             </div>
         </div>
