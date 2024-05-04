0b7bb39cc3f4ff6cb40a24ada09b5bfb1da65515
Inter <hi@iinter.me>
2024-05-04 19:54:19 +0100

    🐛 fix: navbar background

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ function Navbar() {
                 menu {isOpen ? <ChevronDown className="text-emerald-300" /> : <ChevronRight className="text-neutral-600" />}
             </button>
             <div
-                className={`${isOpen ? 'block' : 'hidden'} bg-neutral-900 bg-opacity-60 backdrop-blur-sm p-4 my-2 rounded-md shadow-2xl shadow-neutral-900 z-10 border border-neutral-800 animate-blurred-fade-in duration-300`}
+                className={`${isOpen ? 'block' : 'hidden'} bg-neutral-900 backdrop-blur-sm p-4 my-2 rounded-md shadow-2xl shadow-neutral-900 z-10 border border-neutral-800 animate-blurred-fade-in duration-300`}
             >
                 <div className="flex flex-col space-y-2 text-neutral-300 hover:text-neutral-100 duration-300">
                     <Link href="/" className="text-neutral-100 hover:text-neutral-300 duration-300 flex items-center">
