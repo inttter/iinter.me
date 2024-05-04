89fc2002690051f7aa0ce5db11bbebf505152cea
Inter <hi@iinter.me>
2024-05-04 18:54:10 +0100

    🧹 chore: improve visibility of back button

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -27,7 +27,7 @@ const BackToTop = () => {
 
   return (
     <motion.button
-      className="fixed bottom-6 right-6 text-neutral-600 p-2 bg-neutral-900 hover:bg-zinc-300 hover:text-stone-300 hover:bg-opacity-10 rounded-md duration-300 flex items-center"
+      className="fixed bottom-6 right-6 text-neutral-400 p-2 bg-neutral-800 hover:bg-neutral-700 hover:bg-opacity-80 hover:text-stone-300 rounded-md duration-300 flex items-center"
       initial={{ opacity: 0 }}
       animate={{ opacity: showScrollButton ? 1 : 0 }}
       transition={{ duration: 0.5 }}
