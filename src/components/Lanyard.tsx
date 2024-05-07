4e2fb82d951c7d2fe4a835bede4a54d057be915a
Inter <hi@iinter.me>
2024-05-07 18:01:29 +0100

    🧹 chore: clean up Lanyard component

--- src/components/Lanyard.tsx
+++ src/components/Lanyard.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { FaSpotify } from 'react-icons/fa6';
 import Link from 'next/link';
+import Image from 'next/image';
+import consola from 'consola';
 
 function Lanyard({ showUsername = true, showEmoji = true, showAlbumArt = true }: { showUsername?: boolean, showEmoji?: boolean, showAlbumArt?: boolean }) {
   const [status, setStatus] = useState('✈️ Finding status...');
@@ -14,9 +16,6 @@ function Lanyard({ showUsername = true, showEmoji = true, showAlbumArt = true }:
   useEffect(() => {
     fetch('https://api.lanyard.rest/v1/users/514106760299151372')
       .then(response => {
-        if (!response.ok) {
-          throw new Error('Could not fetch from Lanyard.');
-        }
         return response.json();
       })
       .then(data => {
@@ -24,13 +23,13 @@ function Lanyard({ showUsername = true, showEmoji = true, showAlbumArt = true }:
         let statusDot: JSX.Element | null = null;
 
         if (discordStatus === 'online') {
-          statusDot = <span className="h-4 w-4 rounded-full bg-green-500 inline-block"></span>;
+          statusDot = <span className="h-4 w-4 rounded-full bg-green-500 inline-block tooltip tooltip-top tracking-tight" data-tip="Online" data-theme="lofi"></span>;
         } else if (discordStatus === 'idle') {
-          statusDot = <span className="h-4 w-4 rounded-full bg-amber-400 inline-block"></span>;
+          statusDot = <span className="h-4 w-4 rounded-full bg-amber-400 inline-block tooltip tooltip-top tracking-tight" data-tip="Idle" data-theme="lofi"></span>;
         } else if (discordStatus === 'dnd') {
-          statusDot = <span className="h-4 w-4 rounded-full bg-red-500 inline-block"></span>;
+          statusDot = <span className="h-4 w-4 rounded-full bg-red-500 inline-block tooltip tooltip-top tracking-tight" data-tip="Do Not Disturb" data-theme="lofi"></span>;
         } else if (discordStatus === 'offline') {
-          statusDot = <span className="h-4 w-4 rounded-full bg-gray-500 inline-block"></span>;
+          statusDot = <span className="h-4 w-4 rounded-full bg-gray-500 inline-block tooltip tooltip-top tracking-tight" data-tip="Offline" data-theme="lofi"></span>;
         }
 
         setEmoji(showEmoji ? statusDot : null);
@@ -52,64 +51,33 @@ function Lanyard({ showUsername = true, showEmoji = true, showAlbumArt = true }:
         }
       })
       .catch(error => {
-        console.error('Error fetching Discord status:', error);
-        setStatus('🤷‍♂️ Couldn\'t find a status');
+        consola.error('An error occurred when trying to fetch a response from Lanyard:', error);
+        setStatus('🤷‍♂️ Couldn\'t find a status!');
       });
   }, [showEmoji]);
 
   return (
     <motion.div
-      initial={{ opacity: 0, y: 20 }}
+      initial={{ opacity: 0, y: 5 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5, delay: 0.4 }}
       className={`relative ${showAlbumArt && spotifyAlbumArt ? 'md:w-11/12 w-full bg-opacity-5 bg-[#FAFAFA] border border-neutral-700 rounded-lg p-2' : ''}`}
     >
       {showUsername && (
-        <motion.span
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
-          className="font-semibold text-zinc-100 justify-start tracking-tight"
-        >
+        <span className="font-semibold text-zinc-100 justify-start tracking-tight">
           Inter
-        </motion.span>
+        </span>
       )}
       {emoji && showEmoji && (
-        <motion.span
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.3 }}
-          style={{ fontSize: '0.3em', verticalAlign: 'middle', paddingLeft: '10px' }}
-        >
+        <span className="animate-blurred-fade-in duration-1000" style={{ fontSize: '0.3em', verticalAlign: 'middle', paddingLeft: '10px' }}>
           {emoji}
-        </motion.span>
+        </span>
       )}
       {showAlbumArt && spotifyAlbumArt && (
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 0.4 }}
-          style={{ display: 'flex', alignItems: 'center' }}
-          className="text-zinc-300"
-        >
-          <motion.img
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.6 }}
-            src={spotifyAlbumArt}
-            className="w-12 h-12 rounded-md duration-300"
-            alt="Album Art"
-            style={{ marginRight: '8px' }}
-          />
+        <div className="flex items-center text-zinc-300">
+          <Image src={spotifyAlbumArt} className="rounded-md animate-blurred-fade-in duration-1000" width={50} height={50} alt="Album Art" style={{ marginRight: '8px' }} />
           <div className="w-full overflow-clip truncate">
-            <motion.a
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5, delay: 0.8 }}
-              className="text-zinc-100 hover:text-zinc-300 duration-300 border-b-2 border-dotted border-neutral-700 hover:border-neutral-500 mx-1"
-            >
-              <Link href={`https://open.spotify.com/track/${spotifyTrackId}`} target="_blank" rel="noopener noreferrer">{spotifySong}</Link>
-            </motion.a>
+            <Link href={`https://open.spotify.com/track/${spotifyTrackId}`} target="_blank" rel="noopener noreferrer" className="text-zinc-100 hover:text-zinc-300 animate-blurred-fade-in duration-700 border-b-2 border-dotted border-neutral-700 hover:border-neutral-500 mx-1">{spotifySong}</Link>
             <motion.p
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
@@ -119,27 +87,17 @@ function Lanyard({ showUsername = true, showEmoji = true, showAlbumArt = true }:
               {spotifyArtist}
             </motion.p>
           </div>
-          <div className="mr-2 flex justify-end items-end space-x-1">
+          <div className="mr-2 flex justify-end items-center">
             <FaSpotify size={30} className="text-green-500" />
           </div>
-        </motion.div>
+        </div>
       )}
       {!showAlbumArt && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.4 }}
-          style={{ display: 'flex', alignItems: 'center' }}
-        >
-          <motion.p
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.6 }}
-            className="text-sm tracking-normal text-neutral-500 justify-start overflow-elipsis"
-          >
+        <div className="flex items-center">
+          <p className="text-sm tracking-normal text-neutral-500 justify-start overflow-elipsis animate-blurred-fade-in duration-1000">
             {status}
-          </motion.p>
-        </motion.div>
+          </p>
+        </div>
       )}
     </motion.div>
   );
