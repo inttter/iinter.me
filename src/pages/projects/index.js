411ea4c866347bd3c82c9a7460daf31d8a40e444
Inter <hi@iinter.me>
2024-05-11 23:14:51 +0100

    🧹 chore: simplify frontend for deprecated project

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -24,53 +24,25 @@ export default function ProjectsPage() {
                   </div>
                 </Link>
               </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-x-3 gap-y-1">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-x-3 gap-y-3">
                 {projectsData.map((project, index) => (
-                  <motion.div
-                    key={index}
-                    className="py-1"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.5, delay: index * 0.1 }}
-                  >
+                  <div key={index} className="animate-blurred-fade-in duration-700">
                     <div className="bg-neutral-800 bg-opacity-20 hover:bg-neutral-500 hover:bg-opacity-5 border border-dashed border-neutral-700 hover:border-neutral-800 duration-300 p-3 rounded-md block antialiased">
                       <div className="flex justify-between items-center">
                         <Link href={project.link} target="_blank" rel="noopener noreferer" className="group" passHref>
                           <span className="flex items-center">
-                            <span className="border-b border-dashed border-neutral-600 hover:border-neutral-500 text-zinc-100 hover:text-zinc-300 duration-300">{project.name}</span> <ArrowUpRight size={15} className="m-1 opacity-50 group-hover:opacity-100 group-hover:translate-x-[1.5px] group-hover:text-zinc-300 duration-200" />
+                            <span className={`border-b border-dashed border-neutral-600 hover:border-neutral-500 ${!project.maintained ? "text-amber-300 hover:text-amber-200 duration-300 tooltip tooltip-top bg-transparent" : "text-zinc-100 hover:text-zinc-300 duration-300 bg-transparent"}`} data-theme={!project.maintained ? "lofi" : ""} data-tip={!project.maintained ? "Updates will no longer be provided, and any issues that arise may not be fixed." : ""}>{project.name}</span> <ArrowUpRight size={15} className="m-1 opacity-50 group-hover:opacity-100 group-hover:translate-x-[1.5px] group-hover:text-zinc-300 duration-200" />
                           </span>
                         </Link>
                         <Link href={project.github} target="_blank" rel="noopener noreferer" passHref>
-                          <div className="mr-1 md:mr-0.5 mt-0.5 text-neutral-500 hover:text-stone-300 duration-300" aria-label="GitHub Repository">
+                          <div className="mr-1 md:mr-0.5 mt-0.5 text-neutral-500 hover:text-stone-300 duration-300">
                             <FaGithub size={18} />
                           </div>
                         </Link>
                       </div>
-                      {!project.maintained && (
-                        <motion.div
-                          className="bg-neutral-950 text-zinc-300 border border-neutral-800 p-1 px-2 mt-2.5 rounded-md text-xs flex items-center tooltip tooltip-top cursor-help"
-                          data-tip="Updates will no longer be provided, and any issues that arise will not be fixed."
-                          data-theme="lofi"
-                          initial={{ opacity: 0 }}
-                          animate={{ opacity: 1 }}
-                          transition={{ duration: 0.5, delay: index * 0.1 }}
-                        >
-                          <div className="hover:blur-[1px] duration-300 flex items-center">
-                            ⚠️ This project is
-                            <span className="text-zinc-50 underline ml-1">
-                              no longer maintained
-                            </span>.
-                          </div>
-                        </motion.div>
-                      )}
-                      <motion.p
-                        className="text-stone-400 text-sm py-2.5"
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ duration: 0.5, delay: index * 0.1 }}
-                      >
+                      <p className="text-stone-400 text-sm py-2.5 animate-blurred-fade-in duration-700">
                         {project.description}
-                      </motion.p>
+                      </p>
                       {project.tags && (
                         <motion.div
                           className="-mx-1 p-1 rounded-md text-xs flex flex-wrap items-center"
@@ -86,7 +58,7 @@ export default function ProjectsPage() {
                         </motion.div>
                       )}
                     </div>
-                  </motion.div>
+                  </div>
                 ))}
               </div>
             </div>
