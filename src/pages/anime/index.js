49dd48aaf87374186000b19f4cd23ebc17eb29dc
Inter <hi@iinter.me>
2024-05-15 22:07:30 +0100

    ♻️ refactor: redesign layout of anime list page

--- src/pages/anime/index.js
+++ src/pages/anime/index.js
@@ -17,7 +17,7 @@ const IndexPage = () => {
     planned: [],
   });
   const [profilePicture, setProfilePicture] = useState('');
-  const [username, setUsername] = useState(''); // State to hold the username
+  const [username, setUsername] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
@@ -63,11 +63,11 @@ const IndexPage = () => {
 
         if (user) {
           setProfilePicture(user.avatar.large);
-          setUsername(user.name); // Set the username
+          setUsername(user.name);
         }
       } catch (error) {
         consola.error('An error occurred:', error);
-        setErrorMessage('Error fetching data. Please try again later.');
+        setErrorMessage('Error fetching list. Check the console for more details, or wait a few minutes and try again.');
       }
     };
 
@@ -95,23 +95,23 @@ const IndexPage = () => {
       <div className="max-w-2xl w-full px-4 py-24 space-y-6 flex-col">
         <div className="flex flex-col space-y-4">
           {watchlist.watching.length > 0 && (
-            <div className="animate-blurred-fade-in duration-300">
+            <div className="animate-blurred-fade-in duration-500">
               <WatchlistCategory title="💻 Watching Currently" list={watchlist.watching} titleColor="text-blue-400" />
             </div>
           )}
           {watchlist.completed.length > 0 && (
-            <div className="animate-blurred-fade-in duration-300">
+            <div className="animate-blurred-fade-in duration-500">
               <WatchlistCategory title="✅ Completed" list={watchlist.completed} titleColor="text-emerald-400" />
             </div>
           )}
           {watchlist.planned.length > 0 && (
-            <div className="animate-blurred-fade-in duration-300">
+            <div className="animate-blurred-fade-in duration-500">
               <WatchlistCategory title="⌚ Plan To Watch" list={watchlist.planned} titleColor="text-violet-400" />
             </div>
           )}
         </div>
         <Head>
-          <title>Anime List | {username}</title>
+          <title>Anime List | Inter</title>
         </Head>
         <Navbar />
         <div className="mt-auto">
@@ -125,7 +125,7 @@ const IndexPage = () => {
           </motion.div>
         )}
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.8 }}>
-          <div className="flex justify-end items-center text-sm text-neutral-600">
+          <div className="flex justify-end items-center text-sm text-neutral-600 animate-blurred-fade-in duration-500">
             {profilePicture && (
               <Link href={`https://anilist.co/user/${username}`} target="_blank" rel="noopener noreferrer" className="group hover:bg-neutral-800 hover:text-zinc-100 p-2 rounded-md duration-300 flex items-center">
                 <Image src={profilePicture} alt="AniList Profile Picture" width={30} height={30} className="rounded-md mr-2.5 -rotate-6 group-hover:scale-110 group-active:scale-105 duration-300" />
@@ -143,36 +143,32 @@ const WatchlistCategory = ({ title, list, titleColor }) => {
   return (
     <div>
       <div className={`text-2xl font-semibold mb-6 tracking-tighter ${titleColor}`}>{title}</div>
-      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
         {list.map(item => (
-          <div key={item.id} className="relative">
-            {item.coverImage && (
-              <div key={item.id} className="group relative hover:shadow-2xl hover:shadow-neutral-700 hover:scale-105 active:scale-95 duration-300">
-                <Link href={`https://anilist.co/anime/${item.id}`} target="_blank" rel="noopener noreferrer">
+          <Link key={item.id} href={`https://anilist.co/anime/${item.id}`} target="_blank" rel="noopener noreferrer" className="group">
+            <div key={item.id} className="relative flex items-center hover:bg-neutral-800 hover:bg-opacity-80 active:bg-neutral-700 active:bg-opacity-45 px-2 py-3 rounded-md duration-300">
+              {item.coverImage && (
+                <div className="group relative">
                   <Image
                     src={item.coverImage}
                     alt={item.title}
-                    width={200}
-                    height={200}
-                    layout="responsive"
-                    className="rounded-md mb-2 opacity-100 transition-opacity duration-300 ease-in-out group-hover:opacity-30 animate-blurred-fade-in"
+                    width={70}
+                    height={70}
+                    layout="intrinsic"
+                    className="rounded-md animate-blurred-fade-in duration-500"
                   />
-                  <span className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 bg-black bg-opacity-50">
-                    <span className="text-zinc-50 font-semibold px-3 text-center md:text-left sm:text-lg md:text-md tracking-tighter">
-                      {item.title}
-                    </span>
+                </div>
+              )}
+              <div className="px-3 flex-grow flex-shrink-0 md:max-w-[500px] max-w-[200px] overflow-elipsis truncate text-zinc-100 md:text-lg text-md font-semibold tracking-tight">
+                {item.title}
+                {title !== "⌚ Plan To Watch" && item.score && item.score > 0 && (
+                  <span className="absolute bottom-1 right-1.5 sm:right-2 md:right-1 bg-neutral-800 group-hover:bg-neutral-950 text-neutral-300 px-2 py-1 rounded-md text-xs font-medium tooltip tooltip-top group-hover:bottom-2 group-hover:right-2 group-hover:md:right-2 duration-300" data-tip="Rating" data-theme="lofi">
+                    {item.score}/10
                   </span>
-                  {title !== "⌚ Plan To Watch" && item.score && item.score > 0 && ( // Check if title is not "Plan To Watch" and score exists and is greater than 0
-                    <span className="absolute bottom-1 right-1.5 sm:right-2 md:right-1 bg-neutral-800 text-neutral-300 px-2 py-1 rounded-md text-xs font-medium tooltip tooltip-top group-hover:bottom-2 group-hover:right-2 group-hover:md:right-2 duration-300" data-tip="Rating" data-theme="lofi">
-                      {item.score}/10
-                    </span>
-                  )}
-                </Link>
+                )}
               </div>
-            )}
-          </div>
+            </div>
+          </Link>
         ))}
-      </div>
       <div className="flex items-center justify-center">
         <hr className="w-full border-t border-neutral-800 mt-2" />
       </div>
