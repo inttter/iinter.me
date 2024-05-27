cee1bbf7d96b6d65ac3893ab0a83152fc8dd5264
Inter <hi@iinter.me>
2024-05-27 16:02:51 +0100

    ✨ feat(404): add button to go back to previous page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import { CircleX } from 'lucide-react';
+import { X, ArrowLeft } from 'lucide-react';
 import Head from 'next/head';
 
 export default function Custom404() {
@@ -9,17 +9,20 @@ export default function Custom404() {
       <Head>
         <title>404 | Inter</title>
       </Head>
-      <div className="max-w-2xl w-full px-4 py-8 space-y-4 flex-col">
+      <div className="max-w-2xl w-full px-4 py-8 space-y-6 flex-col">
         <div className="flex items-center justify-start">
-          <CircleX size={40} className="mr-2 text-red-400 md:mt-0 mt-10" />
-          <h1 className="text-4xl md:text-5xl text-zinc-100 opacity-90 md:mt-0 mt-10 max-w-md font-semibold tracking-tighter border-b-2 border-dotted border-neutral-700 flex items-center">
+          <X size={40} className="mr-2 text-red-400 md:mt-0 mt-10" />
+          <h1 className="text-4xl md:text-5xl text-zinc-100 opacity-90 md:mt-0 mt-10 max-w-md font-semibold tracking-tighter flex items-center">
             Page Not Found
           </h1>
         </div>
-        <p className="text-neutral-500 text-lg max-w-lg ">
+        <p className="text-neutral-500 text-lg max-w-lg">
           Sorry, the page you were looking for could not be found.
-          You might have made a typo, or the link may have been moved/deleted.
+          You may have made a typo, or the link might have been changed, moved, or deleted.
         </p>
+        <button class="group justify-center rounded-md text-sm font-medium bg-neutral-800 text-zinc-300 hover:bg-neutral-700/80 duration-300 px-5 py-2 flex items-center" onClick={() => window.history.back()}>
+          <ArrowLeft size={15} className="mr-1 group-hover:-translate-x-0.5 duration-200" /> Go back
+        </button>
         <Navbar />
       </div>
     </div>
