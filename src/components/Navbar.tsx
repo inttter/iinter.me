b46ea5c939bec30f619913132ef1547e240df2b1
Inter <hi@iinter.me>
2024-06-11 21:06:28 +0100

    ♻️ refactor: significantly improve mobile navbar
    much more nicer now!

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router'; // Import the useRouter hook
-import { ChevronRight, ChevronDown, NotebookPen, Code, Cat, Home } from 'lucide-react';
+import { useRouter } from 'next/router';
+import { ChevronRight, NotebookPen, Code, Cat, Home, List } from 'lucide-react';
 
 function Navbar() {
-    const router = useRouter(); // Initialize the useRouter hook
+    const router = useRouter();
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleMenu = () => {
@@ -13,46 +13,47 @@ function Navbar() {
 
     return (
         <div className="absolute top-10">
-            <button
-                className="md:hidden text-zinc-100 duration-300 flex items-center"
-                onClick={toggleMenu}
-            >
-                menu {isOpen ? <ChevronDown className="text-emerald-300" /> : <ChevronRight className="text-neutral-600" />}
-            </button>
-            <div
-                className={`${isOpen ? 'block' : 'hidden'} bg-neutral-900 backdrop-blur-sm p-4 my-2 rounded-md shadow-2xl shadow-neutral-900 z-10 border border-neutral-800 animate-blurred-fade-in duration-300`}
-            >
-                <div className="flex flex-col space-y-2 text-neutral-300 hover:text-neutral-100 duration-300">
-                    <Link href="/" className="text-neutral-100 hover:text-neutral-300 duration-300 flex items-center">
-                        <Home size={15} className="mr-1.5 text-neutral-600" /> Home
-                    </Link>
-                    <Link href="/anime" className="text-neutral-100 hover:text-neutral-300 duration-300 flex items-center">
-                        <Cat size={15} className="mr-1.5 text-neutral-600" /> Anime List
-                    </Link>
-                    <Link href="/projects" className="text-neutral-100 hover:text-neutral-300 duration-300 flex items-center">
-                       <Code size={15} className="mr-1.5 text-neutral-600" /> Projects
-                    </Link>
-                    <Link href="/writing" className="text-neutral-100 hover:text-neutral-300 duration-300 flex items-center">
-                       <NotebookPen size={15} className="mr-1.5 text-neutral-600" /> Writing
-                    </Link>
-                </div>
-            </div>
-            <div className="hidden md:flex space-x-1 items-center">
-                <Link href="/" className={`text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1 flex items-center ${router.pathname === '/' ? 'bg-neutral-700 bg-opacity-55' : ''}`}>
-                    <Home size={15} className="mr-1.5 text-neutral-500" /> home
-                </Link>
-                <Link href="/anime" className={`text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1 flex items-center ${router.pathname === '/anime' ? 'bg-neutral-700 bg-opacity-55' : ''}`}>
-                   <Cat size={15} className="mr-1.5 text-neutral-500" /> anime
-                </Link>
-                <Link href="/projects" className={`text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1 flex items-center ${router.pathname === '/projects' ? 'bg-neutral-700 bg-opacity-55' : ''}`}>
-                   <Code size={15} className="mr-1.5 text-neutral-500" /> projects
-                </Link>
-                <Link href="/writing" className={`text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1 flex items-center ${router.pathname === '/writing' ? 'bg-neutral-700 bg-opacity-55' : ''}`}>
-                   <NotebookPen size={15} className="mr-1.5 text-neutral-500" /> writing
-                </Link>
-            </div>
+          {/* Mobile Navbar */}
+          <button className={`md:hidden text-zinc-100 px-2 py-1 ${isOpen ? 'bg-neutral-800' : 'bg-transparent'} rounded-md duration-300 flex items-center tags group`} onClick={toggleMenu}>
+            <List size={15} className={`mr-1 text-neutral-500 ${isOpen ? 'text-zinc-300' : 'text-neutral-400'} duration-300`} /> Menu
+              <div className={`transform transition-transform ${isOpen ? 'duration-500 ease-in-out' : 'duration-200 ease-in-out'} ${isOpen ? 'rotate-90 text-emerald-300' : 'rotate-0 text-neutral-600'}`}>
+                <ChevronRight />
+              </div>
+          </button>
+          <div className={`${isOpen ? 'block' : 'hidden'} bg-neutral-900 backdrop-blur-sm p-2.5 my-2 rounded-md shadow-2xl shadow-neutral-900 z-10 border border-neutral-800 animate-blurred-fade-in duration-300`}>
+            <div className="flex flex-col space-y-2 text-neutral-300 hover:text-neutral-100 duration-300">
+              <Link href="/" className="rounded-md text-neutral-100 hover:text-neutral-300 hover:bg-neutral-800 active:bg-neutral-700 active:bg-opacity-50 px-2 py-1 duration-300 flex items-center">
+                <Home size={15} className="mr-1.5 text-neutral-500" /> Home
+              </Link>
+              <Link href="/anime" className="rounded-md text-neutral-100 hover:text-neutral-300 hover:bg-neutral-800 active:bg-neutral-700 active:bg-opacity-50 px-2 py-1 duration-300 flex items-center">
+                <Cat size={15} className="mr-1.5 text-neutral-500" /> Anime List
+              </Link>
+              <Link href="/projects" className="rounded-md text-neutral-100 hover:text-neutral-300 hover:bg-neutral-800 active:bg-neutral-700 active:bg-opacity-50 px-2 py-1 duration-300 flex items-center">
+                <Code size={15} className="mr-1.5 text-neutral-500" /> Projects
+              </Link>
+              <Link href="/writing" className="rounded-md text-neutral-100 hover:text-neutral-300 hover:bg-neutral-800 active:bg-neutral-700 active:bg-opacity-50 px-2 py-1 duration-300 flex items-center">
+                <NotebookPen size={15} className="mr-1.5 text-neutral-500" /> Writing
+              </Link>
+          </div>
         </div>
+        
+        {/* Desktop Navbar */}
+        <div className="hidden md:flex space-x-1 items-center">
+          <Link href="/" className={`text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1 flex items-center ${router.pathname === '/' ? 'bg-neutral-700 bg-opacity-55' : ''}`}>
+            <Home size={15} className="mr-1.5 text-neutral-500" /> home
+          </Link>
+          <Link href="/anime" className={`text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1 flex items-center ${router.pathname === '/anime' ? 'bg-neutral-700 bg-opacity-55' : ''}`}>
+            <Cat size={15} className="mr-1.5 text-neutral-500" /> anime
+          </Link>
+          <Link href="/projects" className={`text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1 flex items-center ${router.pathname === '/projects' ? 'bg-neutral-700 bg-opacity-55' : ''}`}>
+            <Code size={15} className="mr-1.5 text-neutral-500" /> projects
+          </Link>
+          <Link href="/writing" className={`text-zinc-100 duration-300 hover:bg-neutral-800 rounded-md px-2 py-1 flex items-center ${router.pathname === '/writing' ? 'bg-neutral-700 bg-opacity-55' : ''}`}>
+            <NotebookPen size={15} className="mr-1.5 text-neutral-500" /> writing
+          </Link>
+        </div>
+      </div>
     );
-}
+  };
 
 export default Navbar;
