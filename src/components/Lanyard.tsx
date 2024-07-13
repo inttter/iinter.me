bc90f3dc52467922a55b78770e04a5060f15c419
Inter <hi@iinter.me>
2024-07-13 15:51:47 +0100

    🐛 fix(lanyard): improve logic of component
    - Now fetches every 5s to keep updating data
        - May be lengthened later on, depending on how the analytics look.
    
    - Hide progress bar on mobile
        - Until I find a fix for the current timestamp always staying the same as the end timestamp.
    
    - Reset states to initial values if no Spotify data is present in the response.
    
    hopefully nothing is broken here 🙏🙏

--- src/components/Lanyard.tsx
+++ src/components/Lanyard.tsx
@@ -8,74 +8,96 @@ import consola from 'consola';
 function Lanyard({ showUsername = true, showEmoji = true, showAlbumArt = true }) {
   const [status, setStatus] = useState('✈️ Finding status...');
   const [emoji, setEmoji] = useState(null);
-  const [spotifySong, setSpotifySong] = useState('');
-  const [spotifyArtist, setSpotifyArtist] = useState('');
+  const [spotifySong, setSpotifySong] = useState(null);
+  const [spotifyArtist, setSpotifyArtist] = useState(null)
   const [spotifyAlbumArt, setSpotifyAlbumArt] = useState('');
   const [spotifyTrackId, setSpotifyTrackId] = useState('');
   const [spotifyStart, setSpotifyStart] = useState(0);
   const [spotifyEnd, setSpotifyEnd] = useState(0);
   const [currentTime, setCurrentTime] = useState(Date.now());
+  const [isSongPlaying, setIsSongPlaying] = useState(false);
 
   useEffect(() => {
-    fetch('https://api.lanyard.rest/v1/users/514106760299151372')
-      .then(response => {
-        consola.success('%cSuccessfully retrieved data from Lanyard!', 'color: #2ecc71');
-        return response.json();
-      })
-      .then(data => {
-        const discordStatus = data.data.discord_status;
-        let statusDot = null;
-
-        if (discordStatus === 'online') {
-          statusDot = (
-            <span className="h-4 w-4 rounded-full bg-green-500 inline-block md:mt-0 mt-5 mx-2 tooltip tooltip-right" data-tip="Online" data-theme="black"></span>
-          );
-        } else if (discordStatus === 'idle') {
-          statusDot = (
-            <span className="h-4 w-4 rounded-full bg-amber-400 inline-block md:mt-0 mt-5 mx-2 tooltip tooltip-right" data-tip="Idle" data-theme="black"></span>
-          );
-        } else if (discordStatus === 'dnd') {
-          statusDot = (
-            <span className="h-4 w-4 rounded-full bg-red-500 inline-block md:mt-0 mt-5 mx-2 tooltip tooltip-right" data-tip="Do Not Disturb" data-theme="black"></span>
-          );
-        } else if (discordStatus === 'offline') {
-          statusDot = (
-            <span className="h-4 w-4 rounded-full bg-gray-500 inline-block md:mt-0 mt-5 mx-2 tooltip tooltip-right" data-tip="Offline" data-theme="black"></span>
-          );
-        }
-
-        setEmoji(showEmoji ? statusDot : null);
-
-        const gameActivity = data.data.activities.find(activity => activity.type === 0);
-        const gameName = gameActivity ? gameActivity.name : '';
-
-        setStatus(gameName ? `Playing ${gameName}` : '');
-
-        if (data.data.spotify) {
-          setSpotifySong(data.data.spotify.song);
-          setSpotifyArtist(data.data.spotify.artist);
-          if (showAlbumArt && data.data.spotify.album_art_url) {
-            setSpotifyAlbumArt(data.data.spotify.album_art_url);
-          } else {
-            setSpotifyAlbumArt('');
+    const fetchData = () => {
+      fetch('https://api.lanyard.rest/v1/users/514106760299151372')
+        .then(response => response.json())
+        .then(data => {
+          const discordStatus = data.data.discord_status;
+          let statusDot = null;
+  
+          if (discordStatus === 'online') {
+            statusDot = (
+              <span className="h-4 w-4 rounded-full bg-green-500 inline-block md:mt-0 mt-5 mx-2 tooltip tooltip-right" data-tip="Online" data-theme="black"></span>
+            );
+          } else if (discordStatus === 'idle') {
+            statusDot = (
+              <span className="h-4 w-4 rounded-full bg-amber-400 inline-block md:mt-0 mt-5 mx-2 tooltip tooltip-right" data-tip="Idle" data-theme="black"></span>
+            );
+          } else if (discordStatus === 'dnd') {
+            statusDot = (
+              <span className="h-4 w-4 rounded-full bg-red-500 inline-block md:mt-0 mt-5 mx-2 tooltip tooltip-right" data-tip="Do Not Disturb" data-theme="black"></span>
+            );
+          } else if (discordStatus === 'offline') {
+            statusDot = (
+              <span className="h-4 w-4 rounded-full bg-gray-500 inline-block md:mt-0 mt-5 mx-2 tooltip tooltip-right" data-tip="Offline" data-theme="black"></span>
+            );
           }
-          setSpotifyTrackId(data.data.spotify.track_id);
-          setSpotifyStart(data.data.spotify.timestamps.start);
-          setSpotifyEnd(data.data.spotify.timestamps.end);
-          setCurrentTime(Date.now());
-        }
-      })
-      .catch(error => {
-        consola.error('An error occurred when trying to fetch a response from Lanyard:', error);
-        setStatus('🤷‍♂️ No Status');
-      });
+  
+          setEmoji(showEmoji ? statusDot : null);
+  
+          const gameActivity = data.data.activities.find(activity => activity.type === 0);
+          const gameName = gameActivity ? gameActivity.name : '';
+  
+          setStatus(gameName ? `Playing ${gameName}` : '');
+  
+          if (data.data.spotify) {
+            setSpotifySong(data.data.spotify.song);
+            setSpotifyArtist(data.data.spotify.artist);
+            if (showAlbumArt && data.data.spotify.album_art_url) {
+              setSpotifyAlbumArt(data.data.spotify.album_art_url);
+            } else {
+              setSpotifyAlbumArt('');
+            }
+            setSpotifyTrackId(data.data.spotify.track_id);
+            setSpotifyStart(data.data.spotify.timestamps.start);
+            setSpotifyEnd(data.data.spotify.timestamps.end);
+            setIsSongPlaying(true);
+          } else {
+            // if no Spotify data, reset states to initial values
+            setSpotifySong(null);
+            setSpotifyArtist(null);
+            setSpotifyAlbumArt('');
+            setSpotifyTrackId('');
+            setSpotifyStart(0);
+            setSpotifyEnd(0);
+            setIsSongPlaying(false);
+          }
+        })
+        .catch(error => {
+          consola.error('An error occurred when trying to fetch a response from Lanyard:', error);
+          setStatus('🤷‍♂️ Unknown Status');
+        });
+    };
+  
+    fetchData();
 
-    const interval = setInterval(() => {
+    const interval = setInterval(fetchData, 5000);
+  
+    return () => {
+      clearInterval(interval);
+      setIsSongPlaying(false);
+    };
+  }, [showEmoji, showAlbumArt]);
+
+  useEffect(() => {
+    const updateCurrentTime = () => {
       setCurrentTime(Date.now());
-    }, 1000);
-
+    };
+  
+    const interval = setInterval(updateCurrentTime, 1000);
+  
     return () => clearInterval(interval);
-  }, [showEmoji]);
+  }, []);
 
   const formatTimestamp = (timestamp) => {
     const minutes = Math.floor(timestamp / 60000);
@@ -84,12 +106,16 @@ function Lanyard({ showUsername = true, showEmoji = true, showAlbumArt = true })
   };
 
   const calculateProgress = () => {
-    const progress = ((currentTime - spotifyStart) / (spotifyEnd - spotifyStart)) * 100;
+    if (!isSongPlaying || spotifyEnd === 0 || spotifyEnd <= spotifyStart) {
+      return 0;
+    }
+
+    const current = Math.min(Date.now(), spotifyEnd);
+    const progress = ((current - spotifyStart) / (spotifyEnd - spotifyStart)) * 100;
     return Math.min(Math.max(progress, 0), 100);
   };
 
-  const currentTimestamp = Math.min(currentTime, spotifyEnd);
-  const elapsed = currentTimestamp - spotifyStart;
+  const elapsed = Math.min(currentTime - spotifyStart, spotifyEnd - spotifyStart);
 
   return (
     <motion.div
@@ -108,7 +134,7 @@ function Lanyard({ showUsername = true, showEmoji = true, showAlbumArt = true })
           {emoji}
         </span>
       )}
-      {showAlbumArt && spotifyAlbumArt && (
+      {showAlbumArt && spotifyAlbumArt && spotifySong && spotifyArtist && (
         <div className="group">
           <Link href={`https://open.spotify.com/track/${spotifyTrackId}`} target="_blank" rel="noopener noreferrer">
             <motion.div
@@ -137,13 +163,14 @@ function Lanyard({ showUsername = true, showEmoji = true, showAlbumArt = true })
                 >
                   by <span className="font-semibold ml-1">{spotifyArtist}</span> <FaSpotify className="ml-1" />
                 </motion.p>
-                <div className="flex items-center justify-between mt-1 ml-1">
+                {/* progress bar hidden on mobile because current timestamp always becomes end timestamp?? */}
+                <div className="hidden items-center justify-between mt-1 ml-1 md:flex">
                   <span className="text-sm text-stone-500">{formatTimestamp(elapsed)}</span>
                   <div className="relative w-full mx-2 h-2 bg-neutral-800 rounded-full">
                     <motion.div
                       initial={{ width: 0 }}
                       animate={{ width: `${calculateProgress()}%` }}
-                      transition={{ duration: 1, ease: 'linear' }}
+                      transition={{ duration: 0.5, ease: 'linear' }}
                       className="absolute h-full bg-stone-500 rounded-full"
                     />
                   </div>
@@ -170,4 +197,4 @@ function Lanyard({ showUsername = true, showEmoji = true, showAlbumArt = true })
   );
 }
 
-export default Lanyard;                               
+export default Lanyard;
