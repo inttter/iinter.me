50e0c3995c25f15e6fdcb16825c1ed661db1f25b
Inter <hi@iinter.me>
2024-08-05 14:24:12 +0100

    🧹 chore: remove speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { AppProps } from 'next/app';
 import '../styles/index.css';
 import Head from '../components/Layout';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import '@fontsource/geist-sans';
 import '@fontsource/geist-sans/700.css';
 import '@fontsource/geist-sans/600.css';
@@ -17,7 +16,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <div className="selection:bg-neutral-700 selection:text-zinc-300">
           <Component {...pageProps} />
         </div>
-        <SpeedInsights />
         <Analytics />
         <Toaster richColors closeButton theme="dark" />
       </Head>
