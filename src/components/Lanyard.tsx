ecfb16ec35b699899147fe14b43ce6743c60fc60
Inter <hi@iinter.me>
2024-08-05 15:59:41 +0100

    🧹 chore(spotify): better indicate song is being played from spotify

--- src/components/Lanyard.tsx
+++ src/components/Lanyard.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import { FaSpotify } from 'react-icons/fa';
 import Image from 'next/image';
 import consola from 'consola';
 import * as Progress from '@radix-ui/react-progress';
@@ -130,8 +129,8 @@ function Lanyard({ showUsername = true, showEmoji = true, showAlbumArt = true })
   };
 
   // On mobile the time elapsed is way further forward than it should be. 
-  // As a workaround, this elapses 'elapsed' 27 seconds back on mobile devices.
-  const elapsed = Math.min((isMobile ? currentTime - 27000 : currentTime) - spotifyStart, spotifyEnd - spotifyStart);
+  // As a workaround, this elapses 'elapsed' 42 (?) seconds back on mobile devices.
+  const elapsed = Math.min((isMobile ? currentTime - 42000 : currentTime) - spotifyStart, spotifyEnd - spotifyStart);
 
   return (
     <motion.div
@@ -176,55 +175,72 @@ function Lanyard({ showUsername = true, showEmoji = true, showAlbumArt = true })
         </div>
       </div>
       {showAlbumArt && spotifyAlbumArt && spotifySong && spotifyArtist && (
-        <div className="group mt-4">
-          <Link href={`https://open.spotify.com/track/${spotifyTrackId}`} target="_blank" rel="noopener noreferrer">
-            <div className="flex items-center hover:bg-neutral-900 border border-transparent hover:border-neutral-700 transform px-2 py-2 -mt-2 rounded-xl transition duration-300 ease-in-out -mx-2.5 md:-mx-2">
-              <Image 
-                src={spotifyAlbumArt}
-                alt="Album Cover"
-                width={70}
-                height={70}
-                style={{ marginRight: '8px' }}
-                className="rounded-md animate-blurred-fade-in duration-1000"
-              />
-              <div className="w-[240px] md:w-[460px]">
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 0.5, delay: 0.4 }}
-                  className="truncate"
-                >
-                  <div className="text-lg text-stone-300 tracking-tight animate-blurred-fade-in duration-300 mx-1 truncate">
-                    {spotifySong}
+        <div>
+          <div className="mt-2 flex items-center text-xs text-stone-400">
+            <div className="relative flex items-center">
+              <div className="relative">
+                <div className="absolute inline-flex h-2 w-2 rounded-full bg-green-500 opacity-75 animate-ping" />
+                <div className="relative h-2 w-2 rounded-full bg-green-500" />
+              </div>
+              <span className="ml-2 flex items-center">
+                Listening on Spotify
+              </span>
+            </div>
+          </div>
+          <div className="group mt-4">
+            <Link href={`https://open.spotify.com/track/${spotifyTrackId}`} target="_blank" rel="noopener noreferrer">
+              <div className="flex items-center hover:bg-neutral-900 border border-transparent hover:border-neutral-700 transform px-2 py-2 -mt-2 rounded-xl transition duration-300 ease-in-out -mx-2.5 md:-mx-2">
+                <Image
+                  src={spotifyAlbumArt}
+                  alt="Album Cover"
+                  width={70}
+                  height={70}
+                  style={{ marginRight: '8px' }}
+                  className="rounded-md animate-blurred-fade-in duration-1000"
+                />
+                <div className="w-[240px] md:w-[460px]">
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.4 }}
+                    className="truncate"
+                  >
+                    <div className="text-lg text-stone-300 tracking-tight animate-blurred-fade-in duration-300 mx-1 truncate">
+                      {spotifySong}
+                    </div>
+                  </motion.div>
+                  <motion.p
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.8 }}
+                    className="text-sm tracking-normal text-stone-500 justify-start truncate px-1 mr-1 flex items-center"
+                  >
+                    by <span className="font-semibold ml-1 truncate">{spotifyArtist}</span>
+                  </motion.p>
+                  <div className="flex items-center justify-between mt-1 ml-1">
+                    <span className="text-sm text-stone-500">
+                      {formatTimestamp(elapsed)}
+                    </span>
+                    <div className="relative w-full mx-2 h-2 bg-neutral-800 rounded-full">
+                      <Progress.Root
+                        style={{ transform: 'translateZ(0)' }}
+                        value={calculateProgress()}
+                        className="absolute w-full h-full"
+                      >
+                        <Progress.Indicator
+                          className="h-full bg-stone-500 rounded-full"
+                          style={{ width: `${calculateProgress()}%` }}
+                        />
+                      </Progress.Root>
+                    </div>
+                    <span className="text-sm text-stone-500">
+                      {formatTimestamp(spotifyEnd - spotifyStart)}
+                    </span>
                   </div>
-                </motion.div>
-                <motion.p
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 0.5, delay: 0.8 }}
-                  className="text-sm tracking-normal text-stone-500 justify-start truncate px-1 mr-1 flex items-center"
-                >
-                  by <span className="font-semibold ml-1">{spotifyArtist}</span> <FaSpotify className="ml-1" />
-                </motion.p>
-                <div className="flex items-center justify-between mt-1 ml-1">
-                  <span className="text-sm text-stone-500">
-                    {formatTimestamp(elapsed)}
-                  </span>
-                  <div className="relative w-full mx-2 h-2 bg-neutral-800 rounded-full">
-                  <Progress.Root
-                    style={{ transform: 'translateZ(0)' }}
-                    value={calculateProgress()} 
-                    className="absolute w-full h-full">
-                      <Progress.Indicator className="h-full bg-stone-500 rounded-full" style={{ width: `${calculateProgress()}%` }} />
-                    </Progress.Root>
-                  </div>
-                  <span className="text-sm text-stone-500">
-                    {formatTimestamp(spotifyEnd - spotifyStart)}
-                  </span>
                 </div>
               </div>
-            </div>
-          </Link>
+            </Link>
+          </div>
         </div>
       )}
     </motion.div>
