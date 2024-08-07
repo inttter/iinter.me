c31664fa3f4ae62e12339f417f4f30253f0d592b
Inter <hi@iinter.me>
2024-08-07 15:47:09 +0100

    💄 style: add slight blur on scroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,15 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <div className="selection:bg-neutral-700 selection:text-zinc-300">
           <Component {...pageProps} />
+          <div className="fixed inset-x-0 top-0 h-16 pointer-events-none">
+            <div
+              className="w-full h-full backdrop-blur-lg"
+              style={{
+                maskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 100%),',
+                WebkitMaskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 100%)'
+              }}
+            />
+          </div>
         </div>
         <Analytics />
         <Toaster richColors closeButton theme="dark" />
